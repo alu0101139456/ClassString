bbeb24dacbdcb088152b6e360d95f5ce912e720d
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-08-25 17:38:49 +0100

    update

--- include/string.hpp
+++ include/string.hpp
@@ -54,13 +54,14 @@ public:
   String &operator+=(const String &str);
   String &operator+=(const char &chr); //fijarse si hasce falta dejarlo como referencia
 
+
   String &replace(int pos, int sz, const String &str);
   String &replace(const String &subStr, const String &newStr2);
   String &replace_all(const String &subStr, const String &newStr2);
 
   String &substr(int pos, int length = npos_);
 
-  std::vector<String> &split(char delimit = ' ');
+  std::vector<String> split(char delimit = ' ');
 
   String join(std::vector<String> &vStr, char joint = ' ');
   //String join(std::vector<String> &vStr, String joint);
@@ -70,7 +71,7 @@ public:
 private:
   void resize(int length);
   Coordenadas find(const String& str); 
-  String makeString(int inicio, int fin);
+  
 };
 
 String operator+(const String &str1, const String &str2);
